188b9efe09e38f4f04245f8b0d4e49be605f72ca
Yilun Du <yilundu@mit.edu>
2017-12-02 17:05:42 -0500

    Added prototype networks file and folder structure

---
+++ train/networks.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+
+class RecurrentEncoder(nn.Module):
+    """Recurrent network for encoding each environmental frame"""
+    def __init__(self):
+        pass
+
+
+class EnvModel(nn.Module):
+    """Network which given an input image frame, predicts the subsequent frame"""
+    def __init__(self):
+        pass
+
+
+class ModelFree(nn.Module):
+    """Model free path for predicting the action and value"""
+    def __init__(self):
+        pass
+
+
+class I3A(nn.Module):
+    def __init__(self):
+        pass
