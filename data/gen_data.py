628bc8a269dbb6e188ced14d69c6b1ee453bba2a
Alexander Yang <yangalexandery@gmail.com>
2017-12-08 22:47:17 +0000

    modified data

--- data/gen_data.py
+++ data/gen_data.py
@@ -25,18 +25,24 @@ while(tot_frames <= data_length):
 
     size = yay.size_x
     if yay.turn > 10:
-        tot_frames += yay.turn - 3
+        tot_frames += yay.turn - 2
         for i in range(3, len(yay.boards)):
-            one_hot = [np.zeros((size, size)) for j in range(5)]
-            one_hot[yay.actions[i - 1] - 1] = np.tile(1.0, (size, size))
-            one_hot = np.array(one_hot)
+            # one_hot = [np.zeros((size, size)) for j in range(5)]
+            # one_hot[yay.actions[i - 1] - 1] = np.tile(1.0, (size, size))
+            # one_hot = np.array(one_hot)
 
             past_frames = np.array([yay.boards[i - 3], yay.boards[i - 2], yay.boards[i - 1]])
-            total_input = np.vstack([past_frames, one_hot])
+            total_input = np.vstack([past_frames, np.tile(yay.actions[i - 1] - 1, (1, size, size))])
             data.append(total_input)
             curr_frame = np.array(yay.boards[i])
             target.append(curr_frame)
 
+        # last frame, an all-black death screen.
+        past_frames = np.array([yay.boards[-3], yay.boards[-2], yay.boards[-1]])
+        total_input = np.vstack([past_frames, np.tile(4, (1, size, size))])
+        data.append(total_input)
+        curr_frame = np.array(np.tile(0, (size, size)))
+        target.append(curr_frame)
     print(yay.turn, " ", tot_frames)
 
 data = (np.array(data)[:data_length]).astype(np.uint8)
